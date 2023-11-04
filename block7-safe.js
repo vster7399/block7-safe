9b32b09bad3026611f953a42d93d488b7d458a34
lv7399@SterlingCS ~ % <lv7399@SterlingCS>
2023-11-04 00:20:06 -0500

    edited variable numbers

--- block7-safe.js
+++ block7-safe.js
@@ -4,16 +4,17 @@ Assign three variables. Each variable contains the corresponding result of calcu
 Add comments throughout the code to explain each step in the process.
 Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
 Please submit either a file with your code or a screenshot of the code, with the pseudocode showing.
-I need to create one const and three var codes. I will put them together to create the full code leaving spaces or adding commas where needed.
+I need to create one const and three var codes. I will create the HTML, CSS, and JS files and link the three. I will put them together to create the full code leaving spaces or adding commas where needed.
 Now, I need to create a list for the numbers in the combination.
 After the list, I need to align everything so it looks neat.
 */
 
 const str4 = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:';
 //Initial message complete
-var code1 = 11+8;
-var code2 = 40/4;
-var code3 = 1*36;
-//Three numbers based on familial ages
-console.log(str4 + code1 + ',' + code2 + ',' + "and" + " " + code3);
+var code1 = 11-1;
+var code2 = 160/4;
+var code3 = 1*39;
+//Three numbers based on information given
+console.log(str4 + code1 + ',' + code2 + ',' + code3);
 //This is displaying  the code correctly
+
