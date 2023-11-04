9b83cd88e6d545535a664d01c3e7951aecf90fca
lv7399@SterlingCS ~ % <lv7399@SterlingCS>
2023-11-03 23:48:47 -0500

    finished edits

---
+++ block7-safe.js
@@ -0,0 +1,19 @@
+/*Before anything else, write the pseudocode of your steps and how you will solve this exercise.
+Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator (+-/*). Each must equal one of the three codes in the combination.
+Add comments throughout the code to explain each step in the process.
+Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+Please submit either a file with your code or a screenshot of the code, with the pseudocode showing.
+I need to create one const and three var codes. I will put them together to create the full code leaving spaces or adding commas where needed.
+Now, I need to create a list for the numbers in the combination.
+After the list, I need to align everything so it looks neat.
+*/
+
+const str4 = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:';
+//Initial message complete
+var code1 = 11+8;
+var code2 = 40/4;
+var code3 = 1*36;
+//Three numbers based on familial ages
+console.log(str4 + code1 + ',' + code2 + ',' + "and" + " " + code3);
+//This is displaying  the code correctly
